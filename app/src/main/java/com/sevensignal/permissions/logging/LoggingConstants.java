c075981703ae8401056a666df9525690fc5b16f3
Aaron Bauman <aaron.bauman@7signal.com>
2020-03-06 09:36:57 -0500

    adds categories for log messages

---
+++ app/src/main/java/com/sevensignal/permissions/logging/LoggingConstants.java
@@ -0,0 +1,8 @@
+package com.sevensignal.permissions.logging;
+
+public class LoggingConstants {
+
+	public static final String PERM             = "Perm";
+	public static final String PERM_APP         = PERM + "App";
+	public static final String PERM_UI			= PERM + "UI";
+}
